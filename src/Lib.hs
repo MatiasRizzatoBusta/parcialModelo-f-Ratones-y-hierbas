c042827dfbb0a4ee1a96c31e04c58ec592ce50c0
MatiasRizzatoBusta <matias.rizzato@gmail.com>
2020-06-01 15:50:02 -0300

    revisar reduceFatFast

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,7 +9,7 @@ data Raton = UnRaton{
 }deriving (Show,Eq)
 
 ------------------------------------PUNTO 1------------------------------------
-cerebro = UnRaton "Cerebro" 9 0.2 ["brucelosis", "sarampión","tuberculosis"]
+cerebro = UnRaton "Cerebro" 9 0.2 ["brucelosis", "sarampion","tuberculosis"]
 bicenterra = UnRaton "Bicenterrata" 256 0.2 []
 huesudo = UnRaton "Huesudo" 4 10 ["obesidad","sinusitis"]
 ------------------------------------PUNTO 2------------------------------------
@@ -22,13 +22,13 @@ hierbaVerde :: String->Hierba
 hierbaVerde terminacion raton = raton{enfermedades = sacoCiertasEnfermedades terminacion (enfermedades raton)}
 
 sacoCiertasEnfermedades :: String->[String]->[String]
-sacoCiertasEnfermedades terminacion enfermedades = filter (terminanEn terminacion) enfermedades
+sacoCiertasEnfermedades terminacion enfermedades = filter (not.terminanEn terminacion) enfermedades
 
 terminanEn :: String->String->Bool
-terminanEn terminacion  = (== terminacion).tomoTerminacion
+terminanEn terminacion  =(== terminacion).(tomoTerminacion (length terminacion))
 
-tomoTerminacion :: String->String
-tomoTerminacion enfermedad = drop ((length enfermedad)-3) enfermedad --asumiendo q todas las termiaciones son de 3 chars sino rip
+tomoTerminacion :: Int->String->String
+tomoTerminacion num enfermedad = drop ((length enfermedad)-num) enfermedad 
 
 alcachofa :: Hierba
 alcachofa raton = raton{peso=disminuyoPeso (peso raton)}
@@ -42,4 +42,23 @@ hierbaZort raton = raton{nombre="Pinky",peso=0,enfermedades=[]}
 
 hierbaDelDiablo :: Hierba
 hierbaDelDiablo raton = raton{peso= max 0 ((peso raton)-0.1),enfermedades = filter ((>10).length) (enfermedades raton) }
+
+------------------------------------PUNTO 3------------------------------------
+
+pondsAntiAge :: Hierba
+pondsAntiAge = hierbaBuena.hierbaBuena.hierbaBuena.alcachofa
+
+reduceFatFast :: Int->Hierba
+reduceFatFast potencia  = (hierbaVerde "idad").(alcachofaPotenciada potencia)--no me esta haciendo alcachofaPotenciada
+
+alcachofaPotenciada :: Int->Hierba
+alcachofaPotenciada potencia  = last.(take (potencia)).(iterate alcachofa)
+--con iterate creo una lista infinita con ratones despues de pasar por alcahcofa,tomo los primeros 2 y de ahi tomo el ultimo
+--que seria el resultado de aplicarle dos veces alcachofa
+
+sufijosInfecciosas = [ "sis", "itis", "emia", "cocos"]
+
+pdepCilina :: Hierba
+pdepCilina raton = foldr ($) raton (map (hierbaVerde) sufijosInfecciosas)--hago una lista con la hierba y cada terminacion
+                                                                        --y le aplico el raton. me salio d 1 #proud
 
