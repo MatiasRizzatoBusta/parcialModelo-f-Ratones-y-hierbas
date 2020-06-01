2eb2083c28d1a43b48e8d45963f57361a032d05f
MatiasRizzatoBusta <matias.rizzato@gmail.com>
2020-06-01 17:03:09 -0300

    subo puntos 5 y 6.Falta terminar 4A y revisar RFF

--- src/Lib.hs
+++ src/Lib.hs
@@ -44,14 +44,15 @@ hierbaDelDiablo :: Hierba
 hierbaDelDiablo raton = raton{peso= max 0 ((peso raton)-0.1),enfermedades = filter ((>10).length) (enfermedades raton) }
 
 ------------------------------------PUNTO 3------------------------------------
+type Medicamento = Hierba
 
-pondsAntiAge :: Hierba
+pondsAntiAge :: Medicamento
 pondsAntiAge = hierbaBuena.hierbaBuena.hierbaBuena.alcachofa
 
-reduceFatFast :: Int->Hierba
+reduceFatFast :: Int->Medicamento
 reduceFatFast potencia  = (hierbaVerde "idad").(alcachofaPotenciada potencia)--no me esta haciendo alcachofaPotenciada
 
-alcachofaPotenciada :: Int->Hierba
+alcachofaPotenciada :: Int->Medicamento
 alcachofaPotenciada potencia  = last.(take (potencia)).(iterate alcachofa)
 --con iterate creo una lista infinita con ratones despues de pasar por alcahcofa,tomo los primeros 2 y de ahi tomo el ultimo
 --que seria el resultado de aplicarle dos veces alcachofa
@@ -62,3 +63,48 @@ pdepCilina :: Hierba
 pdepCilina raton = foldr ($) raton (map (hierbaVerde) sufijosInfecciosas)--hago una lista con la hierba y cada terminacion
                                                                         --y le aplico el raton. me salio d 1 #proud
 
+------------------------------------PUNTO 4------------------------------------
+--Parte A--
+
+--Parte B--
+lograEstabilizar :: Medicamento->[Raton]->Bool
+lograEstabilizar medicamento = estabiliza.(map enfermedades).(map medicamento)
+
+estabiliza :: [[String]]->Bool
+estabiliza listaEnfermedades = sacaObesidad listaEnfermedades && menosDe3Enfermedades listaEnfermedades
+
+sacaObesidad :: [[String]]->Bool
+sacaObesidad enfermedades = all (==True) (map (not.(elem "obesidad")) enfermedades) 
+
+menosDe3Enfermedades :: [[String]]->Bool
+menosDe3Enfermedades enfermedades = all (==True )(map ((<3).length) enfermedades) 
+
+--Parte C--
+buscoPotencia ::Hierba->[Raton]->Int
+buscoPotencia hierba = sum.(map (cantidadIdeal hierba))
+--Armo una lista con la cantidad que requiere cada raton y sumo estos valores.
+
+------------------------------------PUNTO 5------------------------------------
+--Parte A--
+{-
+No va a ser posible saber si el medicamento va a estabilizar a la comunidad ya que este terminaria de aplicarse a la comunidad.
+
+--Parte B--
+En este caso si seria posible ya que buscaria en la lista hasta encontrar el caso que cumple la condicion.si tuviera que buscar
+todos los ratones que quedan asi volvemos al caso A, ya que tendira que analizar una lista infinita
+
+------------------------------------PUNTO 6------------------------------------
+--Parte A--
+para agregar una nueva hierba solo habira que codear lo que deberia hacer sin cambiar nada de lo que ya esta codeado.Lo mismo sucede
+si quiero agregar un nuevo medicamento que utilize esta hierba solamente.En caso de que algun medicamento ya existente utilize esta
+nueva hierba solo habria que pasarselo como paramtero.
+
+--Parte B--
+El concepto aplicado es el de orden superior ya que una funcion recive una funcion como parametro.En este caso, este concepto es el
+que me permite agregar funciones nuevas que pueden ser usadas por otras funciones sin la necesidad de modificar las funciones
+existentes.
+
+--Parte c--
+Solo habria que modificar las funciones que utilizen SOLO el peso del raton.El cambio que habira que hacer es cambiar el tipo de dato
+a uno que se ajuste al nuevo requerimiento
+-}
